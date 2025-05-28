5ba2c3f208981177c7cbbdbe2f602377ad9c987e
Gabriel <163205019+Gabrileu-Galilei@users.noreply.github.com>
2025-05-28 12:19:34 -0300

    Create firstpython.py
    Do not venture to this mediocre file

---
+++ firstpython.py
@@ -0,0 +1,5 @@
+#Whoever you are. You are in the wrong place. If you download this you are insane
+recursions = 700
+for i in range(recursions):
+  print("Why did you tried this code?")
+  print("Nothing here, dude")
